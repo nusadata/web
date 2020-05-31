76eaf72f9ca637b8fa842ab02fc45054c3212c34
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2020-06-01 04:29:34 +0700

    chore: remove vue-client-only

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import ClientOnly from 'vue-client-only'
 import DefaultLayout from '~/layouts/Default.vue'
 import font from './font'
 
@@ -9,7 +8,6 @@ export default function (Vue, { router, head, isClient }) {
   Vue.prototype.$devMode = process.env.GRIDSOME_DEV_MODE === 'true'
   Vue.prototype.$isClient = isClient
   Vue.component('Layout', DefaultLayout)
-  Vue.component('ClientOnly', ClientOnly)
 
   // HEAD manipulation
   head.style.push({
