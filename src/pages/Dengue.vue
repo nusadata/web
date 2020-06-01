d8c8859678e7661334be25b6f517478c5250e96d
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2020-06-01 17:35:36 +0700

    feat: add paragraph section after title

--- src/pages/Dengue.vue
+++ src/pages/Dengue.vue
@@ -1,14 +1,20 @@
 <template>
   <Layout>
     <main role="main">
-      <div class="mt-5 px-5 lg:px-0 py-10 max-w-4xl mx-auto">
-        <h1 class="hidden" aria-hidden="true">{{ meta.title }}</h1>
-        <p class="text-gray-500 font-semibold">2011-2018</p>
-        <p class="text-3xl font-bold">
-        <span class="text-blue-500">Dengue Fever</span> in Indonesia
+      <section class="mt-5 px-5 lg:px-0 py-10 max-w-4xl mx-auto">
+        <header class="mb-2">
+          <h1 class="hidden" aria-hidden="true">{{ meta.title }}</h1>
+          <p class="text-gray-500 font-semibold">2011-2018</p>
+          <p class="text-3xl font-bold">
+            <span class="text-blue-500">Dengue Fever</span> in Indonesia
+          </p>
+        </header>
+        <p class="text-gray-500">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
-      </div>
+      </section>
       <DengueMap :year-range="yearRange"/>
+      <DengueTrend :year-range="yearRange"/>
     </main>
   </Layout>
 </template>
@@ -20,6 +26,7 @@ export default {
   },
   components: {
     DengueMap: () => import('~/components/DengueMap.vue'),
+    DengueTrend: () => import('~/components/DengueTrend.vue'),
   },
   computed: {
     meta() {
