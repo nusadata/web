579951ff680fcffb65c7cfca65e107e0ebe8c27d
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2020-07-08 11:40:10 +0700

    feat: add info on timeline

--- src/pages/CoronavirusTimeline.vue
+++ src/pages/CoronavirusTimeline.vue
@@ -20,6 +20,13 @@
         <h1 class="hidden" aria-hidden="true">{{ meta.title }}</h1>
         <p class="font-bold text-4xl pt-10 mb-2"><span class="text-blue-500">Coronavirus</span> Timeline</p>
         <p class="text-gray-500">This page provides timeline visualization regarding the government actions in response to COVID-19 pandemic starting from the first case to this day. We will update this page regularly following to the latest development of pandemic situation in Indonesia. Best viewed in desktop platform.</p>
+
+        <div class="text-center mt-10">
+          <span class="bg-gray-800 rounded px-3 py-1 text-xs">
+            For best mobile experience, you can visit this <g-link class="underline" to="/coronavirus-calendar/">calendar format</g-link>
+          </span>
+        </div>
+
         <div class="flex flex-col items-center pt-20 pb-10">
           <p class="text-gray-500">Scroll down to navigate</p>
 					<span class="text-gray-500">
