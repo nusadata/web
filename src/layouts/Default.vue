8c81dc1a23b289ac7e76cca1f3d5f76757f6c4dc
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2020-07-08 11:42:56 +0700

    feat: add calendar link on nav

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -9,6 +9,11 @@
           </g-link>
           <div class="flex-1 flex justify-end text-right text-sm">
             <g-link
+              class="py-5 text-gray-500 hover:text-gray-400 mr-5"
+              to="/coronavirus-calendar/">
+              COVID-19 in calendar
+            </g-link>
+            <g-link
               class="py-5 text-gray-500 hover:text-gray-400"
               to="/collections/">
               Collections
